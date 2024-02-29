fb5457242deca7d9fd59ff7132d7c98931ebd0e2
TerraCrafterE3 <terracraftere3@gmail.com>
2024-02-29 19:17:38 +0100

    Added Title

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
 document.addEventListener("mousemove", MouseMove);
 
 function MouseMove(e) {
-    factor = 30;
+    factor = 50;
     x = e.clientX / factor;
     y = e.clientY / factor;
     document.body.style.backgroundPosition = x + "px " + y + "px";
